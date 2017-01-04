e710d11ad6bbf245b77766d698e0c33baf0224d9
Pranav Chandra <chandra.pranav.9@gmail.com>
2017-01-03 23:46:58 -0500

    Connect4 Unity
    Simple 2D Connect 4 game created using Unity3D.
    Game Modes:
    - Single Player (Against AI)
    - Two Player (Same PC)

---
+++ Connect4/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+
+
+	public int columns = 7;
+	public int rows = 7;
+	public GameObject gameTiles;
+	public GameObject buttonTiles;
+
+	private Transform boardHolder;
+
+
+	void boardSetup(){
+		boardHolder = new GameObject ("Board").transform;
+
+
+		int checkColumns = columns * 5 - 15;
+		int checkRows = rows * 5 - 19;
+		int x,y;
+		for (x = -15; x<checkColumns; x=x+5) {
+			for (y = -19; y<checkRows; y=y+5) {
+
+				GameObject toInstantiate = gameTiles;
+				GameObject instance = Instantiate(toInstantiate,new Vector3(x,y,0f),Quaternion.identity) as GameObject;
+
+				instance.transform.SetParent(boardHolder);
+			}
+			GameObject toInstantiateButtons = buttonTiles;
+			GameObject buttonInstance = Instantiate(toInstantiateButtons,new Vector3(x,y,0f),Quaternion.identity) as GameObject;
+			
+			buttonInstance.transform.SetParent(boardHolder);
+			buttonInstance.AddComponent<ButtonClick>();
+		
+		}
+
+	}
+
+	public void setupScene(){
+		boardSetup (); 
+	}
+
+	public void performAction(int i){
+		Debug.Log ("JJJ");
+	}
+}
---
+++ Connect4/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClick : MonoBehaviour {
+	public BoardManager boardManager;
+
+	void Start(){
+
+	}
+
+	void OnMouseUpAsButton(){
+
+		//if (boardManager != null) {
+			boardManager.GetComponent<BoardManager>().performAction(5);
+		/*} else if (boardManager == null){
+			Debug.Log ("Hello");
+		}*/
+	}
+
+}
